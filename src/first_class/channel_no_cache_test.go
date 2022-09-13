33544c4814c87c368435ff9d52f33a05c665c0c0
oldman <lipeng@kachexiongdi.com>
2022-09-13 13:32:56 +0800

    GSP

--- src/first_class/channel_no_cache_test.go
+++ src/first_class/channel_no_cache_test.go
@@ -2,6 +2,7 @@ package first_class
 
 import (
 	"fmt"
+	"sync"
 	"testing"
 	"time"
 )
@@ -28,4 +29,84 @@ func TestChannelNoCache(t *testing.T) {
 	c := spawn01(worker)
 	<-c
 	fmt.Println("worker work done...")
+}
+
+func worker2(i int) {
+	fmt.Printf("worker %d: is working...\n", i)
+	time.Sleep(time.Second)
+	fmt.Printf("worker %d: works done...\n", i)
+}
+
+type signal11 struct {
+}
+
+func spawnGroup(f func(i int), num int, groupSignal <-chan signal11) <-chan signal11 {
+	c := make(chan signal11)
+	var wg sync.WaitGroup
+
+	for i := 0; i < num; i++ {
+		wg.Add(1)
+		go func(i int) {
+			<-groupSignal
+			fmt.Printf("worker %d: start to worker...\n", i)
+			f(i)
+			wg.Done()
+		}(i + 1)
+	}
+	go func() {
+		wg.Wait()
+		c <- signal11{}
+	}()
+	return c
+}
+
+func TestChannelNoCacheOneToN(t *testing.T) {
+	fmt.Println("Start a group of workses...")
+	groupSignal := make(chan signal11)
+	c := spawnGroup(worker2, 5, groupSignal)
+	time.Sleep(5 * time.Second)
+	fmt.Println("the group of workers start to work...")
+	//close(groupSignal)
+	for i := 0; i < 5; i++ {
+		groupSignal <- signal11{}
+	}
+	<-c
+	fmt.Println("the group of workers work done!")
+
+}
+
+type counter struct {
+	c chan int
+	i int
+}
+
+func NewCounter() *counter {
+	cter := &counter{
+		c: make(chan int),
+	}
+	go func() {
+		for {
+			cter.i++
+			cter.c <- cter.i
+		}
+	}()
+	return cter
+}
+
+func (cter *counter) Incres() int {
+	return <-cter.c
+}
+
+func TestChannelNoCacheLock(t *testing.T) {
+	cter := NewCounter()
+	var wg sync.WaitGroup
+	for i := 0; i < 10; i++ {
+		wg.Add(1)
+		go func(i int) {
+			v := cter.Incres()
+			fmt.Printf("goroutine-%d: current counter value is %d\n", i, v)
+			wg.Done()
+		}(i)
+	}
+	wg.Wait()
 }
